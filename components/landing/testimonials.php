47923d98f4427de27c4f48fc9b27150648358565
AntoinetteCuizon <135131114+AntoinetteCuizon@users.noreply.github.com>
2023-06-08 12:48:37 +0700

    Add files via upload

---
+++ components/landing/testimonials.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .__services-main {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .title {
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .main {
+            display: inline-block;
+            position: relative;
+            margin: 15px;
+        }
+
+        .main img {
+            display: block;
+            margin: 0 auto;
+            max-width: 450px;
+        }
+
+    </style>
+</head>
+<body>
+    <section class="__services-main">
+        <div class="__services-header">
+            <span class="title">TESTIMONIALS</span>
+        </div>
+
+        <div class="main">
+            <div class="column">
+                <img src="assets/media/Reviews/rev2.jpg">
+                <div class="bg"></div>
+                <div class="__bottom"></div>
+            </div>
+        </div>
+        
+        <div class="main">
+            <div class="column">
+                <div class="bg"></div>
+                <div class="__bottom">
+                    <img src="assets/media/Reviews/rev3.jpg">
+                </div>
+            </div>
+        </div>
+        
+        <div class="main">
+            <img src="assets/media/Reviews/rev4.jpg">
+            <div class="bg"></div>
+            <div class="__bottom"></div>
+        </div>
+    </section>
+</body>
+</html>
