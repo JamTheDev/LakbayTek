0c7f1b54725ada5f320ee7bc2719e4e3bf790db5
AntoinetteCuizon <135131114+AntoinetteCuizon@users.noreply.github.com>
2023-06-08 11:55:02 +0700

    Add files via upload

---
+++ contact us.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: "Metropolis", sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        section.testimonials {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url("https://asl.law/wp-content/uploads/2022/05/bigstock-Smiling-Group-Of-Diverse-Frien-433580726-scaled.jpg");
+            background-size: cover;
+            background-position: center;
+            color: white;
+            text-align: center;
+        }
+
+        .title {
+            font-family: "Metropolis Black", sans-serif;
+            font-size: 3em;
+            margin-bottom: 20px;
+        }
+
+        .subtitle {
+            font-size: 1.5em;
+            margin-bottom: 50px;
+            letter-spacing: 4px;
+        }
+
+        .contact-info {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 50px;
+        }
+
+        .contact-info a {
+            color: white;
+            text-decoration: none;
+        }
+
+        .contact-info a:hover {
+            text-decoration: underline;
+        }
+
+        .contact-info .phone {
+            font-size: 1.2em;
+            margin-bottom: 20px;
+        }
+
+        .contact-info .message {
+            font-size: 1.2em;
+        }
+    </style>
+</head>
+<body>
+    <section class="testimonials">
+        <h1 class="title">CONTACT US!</h1>
+        <h2 class="subtitle">INQUIRE FOR MORE DETAILS AND INFORMATION</h2>
+
+        <div class="contact-info">
+            <p class="phone">Call us at <a href="tel:09275739292">09275739292</a></p>
+            <p class="message">Send us a message on our <a href="https://www.facebook.com/CasaQuerenciaPrivateHotSpring">Facebook Page</a></p>
+        </div>
+    </section>
+</body>
+</html>
---
+++ regulations.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: Metropolis, sans-serif;
+            background: url("160436561_214257753814392_6448873924224587083_n.jpg") no-repeat center center fixed;
+            background-size: cover;
+            padding: 20px;
+        }
+
+        .testimonials {
+            text-align: center;
+            margin-bottom: 30px;
+            background-color: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        .title {
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        .subtitle {
+            font-size: 16px;
+            font-weight: bold;
+            color: #666666;
+            margin-top: 20px;
+            display: block;
+        }
+
+        .__entry-card-group {
+            display: flex;
+            justify-content: center;
+        }
+
+        .__entry-card {
+            background-color: rgba(255, 255, 255, 0.8);
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            max-width: 500px;
+            width: 100%;
+        }
+
+        .__text-content {
+            font-size: 14px;
+            line-height: 1.6;
+            color: #333333;
+        }
+
+        .__content p {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <section class="testimonials">
+        <div class="__testimonials-titles">
+            <span class="title">REGULATIONS</span><br><br>
+            <span class="subtitle"><b>Do take note of the regulations of the resort upon reserving your slot.</b></span>
+        </div>
+
+        <section class="__entry-card-group">
+            <div class="__entry-card">
+                <div class="__text-content">
+                    <span class="__content">
+                        <p>No cancellations and Refunds</p>
+                        <p>50% Downpayment upon reserving, No reservation fees</p>
+                        <p>Rescheduling is free of charge</p>
+                        <p>Follow the maximum capacity of the package that you have availed, otherwise, you are responsible to avail the package that can accommodate the capacity that you have entered.</p>
+                        <p>Online Payment is available through GCash only</p>
+                    </span>
+                </div>
+            </div>
+        </section>
+    </section>
+</body>
+</html>
