4384d2183e58eb3b6588b5f81f41f4d07b55bc9e
Baeee-017 <135231206+Baeee-017@users.noreply.github.com>
2023-06-08 12:42:16 +0800

    Add files via upload

--- reserve.php
+++ reserve.php
@@ -2,7 +2,11 @@
 <html lang="en">
 <?php
 require_once("config.php");
-
+if (!verifyRememberMeToken()) {
+    header("Location: register.html");
+    exit;
+}
+start_reservation_process();
 ?>
 
 <body>
@@ -82,32 +86,41 @@ require_once("config.php");
     <?php require("components/navbar.php"); ?>
 
     <?php if (verifyRememberMeToken()) : ?>
+
         <form class="main-form" action="" method="post">
             <?php
             $redir_page = $_POST['current_page'] ?? "welcome";
             require("components/reservation/$redir_page.php"); ?>
             <input type="text" class="current_page" name="current_page" value="welcome" hidden>
         </form>
-    <?php endif; ?>
 
-    <script>
-        let form;
-        window.onload = () => {
-            form = document.querySelector(".main-form");
-        }
-
-        function changePage(page) {
-            if (!form) return;
-            if (page == "home") {
-                window.location.href = "index.php";
-                return;
+        <script>
+            let form;
+            window.onload = () => {
+                form = document.querySelector(".main-form");
             }
-            document.querySelector(".current_page").value = page;
-            form.submit();
-        }
-    </script>
+
+            function changePage(page) {
+                if (!form) return;
+                if (page == "home") {
+                    window.location.href = "index.php";
+                    return;
+                }
+                document.querySelector(".current_page").value = page;
+                form.submit();
+            }
+
+            function saveToCookie(data) {
+                fetch('./api/local_save_reservation.php', {
+                    method: "POST",
+                    header: "Content-Type: application/json",
+                    body: JSON.stringify(data)
+                }).then((r) => console.log(r.statusText));
+            }
+        </script>
+
+
+
 </body>
 
 </html>
-
-<?php ?>
