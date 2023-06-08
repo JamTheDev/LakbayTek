49e158e32708fb95a8a7e94a9d28b13173e9b052
ocampoella <132764919+ocampoella@users.noreply.github.com>
2023-06-08 10:36:23 +0800

    Add files via upload

---
+++ components/landing/regulations.php
@@ -0,0 +1,20 @@
+<section class="testimonials">
+    <div class="__testimonials-titles">
+        <span class="title">REGULATIONS</span>
+        <span class="subtitle">Do take note of the regulations of the resort upon reserving your slot.</span>
+    </div>
+
+    <section class="__entry-card-group">
+        <div class="__entry-card">
+            <div class="__text-content">
+                <span class="__content">
+                    <br><br><p font-family="Metropolis Black">-  No cancellations and Refunds <br>
+                 - 50% Downpayment upon reserving, No reservation fees </br>
+                 - Rescheduling is free of charge <br>
+                 - Follow the maximum capacity of the package that you have availed, otherwise, you are responsible to avail the package that can accommodate the capacity that you have entered. <br>
+                 - Online Payment is available through GCash only <br></p>
+                 </span>
+            </div>
+        </div>
+    </section>
+</section>
---
+++ components/landing/testimonials.php
@@ -0,0 +1,32 @@
+<style>
+    @import url("styles/services.css");
+</style>
+
+<section class="__services-main">
+    <div class="__services-header">
+        <span class="title">TESTIMONIALS</span>
+    </div>
+
+            <div class="main">
+                <div class=".column">
+            <img src="assets/media/Reviews/rev2.jpg">
+                <div class="bg">&nbsp;</div>
+                <div class="__bottom">
+                </div>
+            </div>
+            <div class="main">
+            <div class=".column">
+                <div class="bg">&nbsp;</div>
+                <div class="__bottom">
+                <img src="assets/media/Reviews/rev3.jpg">
+                </div>
+            </div>
+            <div class="main">
+            <img src="assets/media/Reviews/rev4.jpg">
+                <div class="bg">&nbsp;</div>
+                <div class="__bottom">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
